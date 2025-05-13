9fa15d8581dbcbdca26f274bd2b3ad3bbcc042c8
Teemo <58159901+liqideqq@users.noreply.github.com>
2025-05-13 16:25:41 +0800

    Update PmsHookTarget34.kt

--- xposed/src/main/java/com/android/hmal/xposed/hook/PmsHookTarget34.kt
+++ xposed/src/main/java/com/android/hmal/xposed/hook/PmsHookTarget34.kt
@@ -1,21 +1,19 @@
 package com.android.hmal.xposed.hook
 
 import android.annotation.TargetApi
-import android.os.Binder
 import android.os.Build
 import com.github.kyuubiran.ezxhelper.utils.findMethod
-import com.github.kyuubiran.ezxhelper.utils.findMethodOrNull
 import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import de.robv.android.xposed.XC_MethodHook
 import com.android.hmal.common.Constants
 import com.android.hmal.xposed.*
 import java.util.concurrent.atomic.AtomicReference
 
-@TargetApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+@TargetApi(Build.VERSION_CODES.TIRAMISU)
 class PmsHookTarget34(private val service: HMALService) : IFrameworkHook {
 
     companion object {
-        private const val TAG = "PmsHookTarget34"
+        private const val TAG = "HMAL-PHT34"
     }
 
     private val getPackagesForUidMethod by lazy {
@@ -25,12 +23,10 @@ class PmsHookTarget34(private val service: HMALService) : IFrameworkHook {
     }
 
     private var hook: XC_MethodHook.Unhook? = null
-    private var exphook: XC_MethodHook.Unhook? = null
     private var lastFilteredApp: AtomicReference<String?> = AtomicReference(null)
 
     @Suppress("UNCHECKED_CAST")
     override fun load() {
-        logI(TAG, "Load hook")
         hook = findMethod("com.android.server.pm.AppsFilterImpl", findSuper = true) {
             name == "shouldFilterApplication"
         }.hookBefore { param ->
@@ -45,42 +41,11 @@ class PmsHookTarget34(private val service: HMALService) : IFrameworkHook {
                 for (caller in callingApps) {
                     if (service.shouldHide(caller, targetApp)) {
                         param.result = true
-                        service.filterCount++
                         val last = lastFilteredApp.getAndSet(caller)
-                        if (last != caller) logI(TAG, "@shouldFilterApplication: query from $caller")
-                        logD(TAG, "@shouldFilterApplication caller: $callingUid $caller, target: $targetApp")
                         return@hookBefore
                     }
                 }
             }.onFailure {
-                logE(TAG, "Fatal error occurred, disable hooks", it)
-                unload()
-            }
-        }
-        // AOSP exploit - https://github.com/aosp-mirror/platform_frameworks_base/commit/5bc482bd99ea18fe0b4064d486b29d5ae2d65139
-        // Only 14 QPR2+ has this method
-        exphook = findMethodOrNull("com.android.server.pm.PackageManagerService", findSuper = true) {
-            name == "getArchivedPackageInternal"
-        }?.hookBefore { param ->
-            runCatching {
-                val callingUid = Binder.getCallingUid()
-                if (callingUid == Constants.UID_SYSTEM) return@hookBefore
-                val callingApps = Utils.binderLocalScope {
-                    service.pms.getPackagesForUid(callingUid)
-                } ?: return@hookBefore
-                val targetApp = param.args[0].toString()
-                for (caller in callingApps) {
-                    if (service.shouldHide(caller, targetApp)) {
-                        param.result = null
-                        service.filterCount++
-                        val last = lastFilteredApp.getAndSet(caller)
-                        if (last != caller) logI(TAG, "@getArchivedPackageInternal: query from $caller")
-                        logD(TAG, "@getArchivedPackageInternal caller: $callingUid $caller, target: $targetApp")
-                        return@hookBefore
-                    }
-                }
-            }.onFailure {
-                logE(TAG, "Fatal error occurred, disable hooks", it)
                 unload()
             }
         }
@@ -89,7 +54,5 @@ class PmsHookTarget34(private val service: HMALService) : IFrameworkHook {
     override fun unload() {
         hook?.unhook()
         hook = null
-        exphook?.unhook()
-        exphook = null
     }
 }
